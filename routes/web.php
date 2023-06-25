00798d25a4dd9d15872e0e005baf5acf41a91adf
MuhammadDavidSyaputra <davidsyaputra979@gmail.com>
2023-06-25 12:40:23 +0700

    commit

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,29 @@ Route::get('/', function () {
 Route::get('/', function () {
     return view('index');
 });
+
+
+Route::get('/main', function () {
+    return view('main');
+});
+
+Route::get('/buku', function () {
+    return view('buku');
+});
+
+
+Route::get('/peminjam', function () {
+    return view('peminjam');
+});
+
+Route::get('/pengembalian', function () {
+    return view('pengembalian');
+});
+
+Route::get('/user', function () {
+    return view('user');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
